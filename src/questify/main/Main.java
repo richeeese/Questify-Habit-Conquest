5edaddaca377d643a63e908039a0d6b6db76a418
chizu <ritzycelestial@gmail.com>
2025-11-20 16:21:10 +0800

    Start of making project flow (Main class)

---
+++ src/questify/main/Main.java
@@ -0,0 +1,15 @@
+package questify.main;
+
+import questify.models.*;
+import questify.logic.*;
+import questify.ui.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+        ConsoleMenu menu = new ConsoleMenu(); // Initialize the console menu
+        menu.start(); // Start the menu interaction
+
+        
+    }
+}
